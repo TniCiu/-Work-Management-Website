060fc31c8e146f6f5ef7ba681c737185446e7ad5
TniCiu <tincui012@gmail>
2025-03-10 22:53:13 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Đảm bảo base path đúng khi deploy
+  base: '/', // Đảm bảo base path đúng khi deploy
   plugins: [
     react(),
     svgr()
