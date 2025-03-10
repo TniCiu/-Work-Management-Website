43b9ba88b622a0b9c792604787db70e7f94197aa
TniCiu <tincui012@gmail>
2025-03-10 23:03:19 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: './',  // Sửa từ '/' thành './' để đảm bảo file tĩnh được load đúng
+  base: '/', // Sửa lại từ './' thành '/'
   build: {
     outDir: 'dist',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
   },
   plugins: [
     react(),
-    svgr()
+    svgr(),
   ],
   resolve: {
-    alias: [
-      { find: '~', replacement: '/src' }
-    ]
-  }
-})
+    alias: [{ find: '~', replacement: '/src' }],
+  },
+});
