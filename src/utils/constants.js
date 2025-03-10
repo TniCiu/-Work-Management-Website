9c12b9ac2902020808e559046fd305be7971142d
TniCiu <tincui012@gmail>
2025-03-10 21:05:58 +0700

    Update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@
 
 // Những domain được phép truy cập tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-    'https://work-management-website-1yxj.vercel.app',
+    'https://work-management-website.vercel.app/',
     // Về sau này sẽ deloy lên domain chính thức
 ]
